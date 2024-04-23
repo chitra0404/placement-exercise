3bc4ec346dd76ac271a6269e113accdd8627c912
chitra0404 <chitrasuresh0404@gmail.com>
2024-04-23 21:31:43 +0530

    commit

---
+++ day-8/exercise.js
@@ -0,0 +1,131 @@
+//Create a JSON object representing a person with properties like name, age, and address. Add methods to the object to calculate the person's birth year and full address.
+
+{
+    "name": "John Doe",
+    "age": 30,
+    "address": {
+      "street": "123 Main St",
+      "city": "Exampletown",
+      "state": "ABC",
+      "zipcode": "12345"
+    },
+    "getBirthYear": function() {
+      var currentYear = new Date().getFullYear();
+      return currentYear - this.age;
+    },
+    "getFullAddress": function() {
+      return this.address.street + ", " + this.address.city + ", " + this.address.state + " " + this.address.zipcode;
+    }
+  }
+
+  //2.
+  function mergeJSONObjects(obj1, obj2) {
+  var mergedObj = {};
+
+  // Merge properties from obj1
+  for (var prop1 in obj1) {
+    if (obj1.hasOwnProperty(prop1)) {
+      mergedObj[prop1] = obj1[prop1];
+    }
+  }
+
+  // Merge properties from obj2, handling conflicts
+  for (var prop2 in obj2) {
+    if (obj2.hasOwnProperty(prop2)) {
+      if (mergedObj.hasOwnProperty(prop2)) {
+        // Handle conflict by appending a suffix to the property name
+        var newProp = prop2 + "_merged";
+        mergedObj[newProp] = obj2[prop2];
+      } else {
+        mergedObj[prop2] = obj2[prop2];
+      }
+    }
+  }
+
+  return mergedObj;
+}
+
+//3
+function deepCloneJSONObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
+  }
+  var originalObj = {
+    "name": "John Doe",
+    "age": 30,
+    "address": {
+      "street": "123 Main St",
+      "city": "Exampletown"
+    }
+  };
+  
+  var clonedObj = deepCloneJSONObject(originalObj);
+  
+  console.log(clonedObj);
+
+  //4.
+  function getValueAtPath(obj, path) {
+    var pathArray = path.split('.');
+    var value = obj;
+  
+    try {
+      for (var i = 0; i < pathArray.length; i++) {
+        var key = pathArray[i];
+        value = value[key];
+      }
+      return value;
+    } catch (error) {
+      return undefined;
+    }
+  }
+
+  var person = {
+    "name": "John Doe",
+    "age": 30,
+    "address": {
+      "street": "123 Main St",
+      "city": "Exampletown"
+    }
+  };
+  
+  var path = "address.city";
+  var value = getValueAtPath(person, path);
+  console.log(value); // Output: Exampletown
+  
+  path = "address.zipcode";
+  value = getValueAtPath(person, path);
+  console.log(value); // Output: undefined (invalid path)
+
+  //5
+  function flattenJSONObject(obj, prefix = '') {
+    var flattenObj = {};
+  
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        var newPath = prefix + key;
+        
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+          var nestedObj = flattenJSONObject(obj[key], newPath + '.');
+          Object.assign(flattenObj, nestedObj);
+        } else {
+          flattenObj[newPath] = obj[key];
+        }
+      }
+    }
+  
+    return flattenObj;
+  }
+  var nestedObj = {
+    "name": "John Doe",
+    "age": 30,
+    "address": {
+      "street": "123 Main St",
+      "city": "Exampletown",
+      "country": {
+        "name": "United States",
+        "code": "US"
+      }
+    }
+  };
+  
+  var flattenedObj = flattenJSONObject(nestedObj);
+  console.log(flattenedObj);
